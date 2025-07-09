7e5f14fafd99511b16ac7dae11e1f8674f66c548
kozae00 <rhwogus777@gmail.com>
2025-07-09 16:15:43 +0900

    init

---
+++ backend/src/main/java/com/example/web_lol_be/WebLolBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_lol_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLolBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebLolBeApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/web_lol_be/WebLolBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_lol_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLolBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
