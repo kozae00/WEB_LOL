de5683ee8cf70631954854e1e34695070ba28167
kozae00 <rhwogus777@gmail.com>
2025-07-10 15:25:11 +0900

    feat: Match 도메인 생성

--- backend/src/main/java/com/example/web_lol_be/domain/match/entity/Match.java
+++ backend/src/main/java/com/example/web_lol_be/domain/match/entity/Match.java
@@ -1,4 +1,15 @@
 package com.example.web_lol_be.domain.match.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Match {
+    @Id
+    private String matchId;
+    private LocalDateTime gameStartTime;
+    private String gameMode;
+    private int duration;
 }
